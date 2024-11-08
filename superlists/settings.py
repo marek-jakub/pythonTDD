ab1ccf847e6ddcd98b8b45a70a2f1dee1dc16ac4
marek-jakub <marekjakub@hotmail.co.uk>
2024-11-08 20:15:10 +0100

    Move sqlite database outside of main source tree.

--- superlists/settings.py
+++ superlists/settings.py
@@ -10,10 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +76,7 @@ WSGI_APPLICATION = "superlists.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
