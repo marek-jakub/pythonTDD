91fa2d2d69d2149e1452e96ee0852eeb6125e927
marek-jakub <marekjakub@hotmail.co.uk>
2024-11-21 22:03:09 +0100

    Added implementation of test list item validation FT.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -28,6 +28,16 @@ class FunctionalTest(StaticLiveServerTestCase):
                 rows = table.find_elements(By.TAG_NAME, 'tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
